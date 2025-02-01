246668615a10ff0fc4783cead02e8c9cbbd9a92d
Zero-z-z <chistiakowa.in@yandex.ru>
2025-02-01 12:43:41 +0500

    g

--- Списки/test.py
+++ Списки/test.py
@@ -2,3 +2,5 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QRadioButton, QPushButton, QLabel, QListWidget, QLineEdit)
 
 from main import *
+
+
