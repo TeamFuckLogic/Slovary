54052f27ce5ab69c066237a32fba5370240c4d0c
Zero-z-z <chistiakowa.in@yandex.ru>
2025-02-11 14:10:46 +0500

    мвм

--- Списки/final.py
+++ Списки/final.py
@@ -14,22 +14,37 @@ class FinalWindow(QWidget):
         self.resize(300, 200)
         layout = QVBoxLayout()
         
-
         result_label = QLabel(f"Вы ответили правильно на {self.score} из {self.total} вопросов.")
-        
         time_label = QLabel(f"Время прохождения: {self.elapsed_time}")  
+        
+        
+        grade = self.get_grade(self.score)
+        grade_label = QLabel(f"Ваша оценка: {grade}")
         
         layout.addWidget(result_label)
         layout.addWidget(time_label)
+        layout.addWidget(grade_label) 
         
         exit_button = QPushButton("Закрыть")
         exit_button.clicked.connect(self.close)
         layout.addWidget(exit_button)
         self.setLayout(layout)
 
+    def get_grade(self, score):
+        if 0 <= score <= 3:
+            return 2
+        elif 4 <= score <= 6:
+            return 3
+        elif 7 <= score <= 8:
+            return 4
+        elif 9 <= score <= 10:
+            return 5
+        else:
+            return "Недопустимый балл"  
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    score = 3 
+    score = 3  
     total = 5 
     elapsed_time = "00:00:00" 
     final_window = FinalWindow(score, total, elapsed_time)
